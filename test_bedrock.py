5e31b990fe2eb4aa2013e5bf01d5fb3d024ebf97
wgreunke <wgreunke@gmail.com>
2024-12-29 21:49:44 -0800

    This is working and gets a result back.

--- test_bedrock.py
+++ test_bedrock.py
@@ -1,9 +1,9 @@
 import boto3
 import json
-model_id = "amazon.nova-pro-v1:0"
+#model_id = "amazon.nova-pro-v1:0"
 
 bedrock_client = boto3.client(service_name='bedrock-runtime', region_name='us-east-1') 
-model_id = 'anthropic.claude-v2'  # Replace with the desired model ID
+#model_id = 'anthropic.claude-v2'  # Replace with the desired model ID
 
 prompt3="""This is a picture of a product from a website.  Tell me the:
 <model_number>
@@ -31,21 +31,31 @@ prompt2="Tell me what this picture is about:"
 prompt = "Tell me a joke"
 
 payload = {
-    "anthropic_version": "bedrock-2023-05-31",
-    "max_tokens": 200,
     "messages": [
         {
             "role": "user",
-            "content": prompt
+            "content": [
+                {
+                    "text": "Tell me a joke"
+                }
+            ]
         }
-    ]
+    ],
+    "inferenceConfig": {
+        "max_new_tokens": 1000
+    }
 }
 
+
 response = bedrock_client.invoke_model(
-    modelId=model_id,
+    modelId="amazon.nova-pro-v1:0",
     body=json.dumps(payload)
 )
 
 result = json.loads(response['body'].read())
-generated_text = result['content'][0]['text']  # Updated to match Claude's response format
-print(f"Response: {generated_text}")
+#generated_text = result['outputs'][0]['text']  # Nova-Pro uses 'outputs' instead of 'content'
+
+#Full dump of the response
+print("Response Structure:", json.dumps(result, indent=2))
+
+#print(f"Response: {generated_text}")
