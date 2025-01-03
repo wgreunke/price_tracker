2803c2714a2e6c931ec7344daaa4a2bf8f2f98d9
wgreunke <wgreunke@gmail.com>
2025-01-03 11:02:33 -0800

    This is able to call the nova model.  Note messages is not burried in the request.

--- nova.py
+++ nova.py
@@ -8,23 +8,34 @@ from botocore.exceptions import ClientError
 # Create a Bedrock Runtime client in the AWS Region of your choice.
 client = boto3.client("bedrock-runtime", region_name="us-east-1")
 
+#The titan model and the noval model have different request structure.
+
+
 # Set the model ID, e.g., Titan Text Premier.
 model_id = "amazon.titan-text-premier-v1:0"
+model_id="amazon.nova-pro-v1:0" 
 
 # Define the prompt for the model.
 prompt = "Describe the purpose of a 'hello world' program in one line."
 
 # Format the request payload using the model's native structure.
-native_request = {
-    "inputText": prompt,
-    "textGenerationConfig": {
-        "maxTokenCount": 512,
-        "temperature": 0.5,
-    },
-}
+#Copoied from model playground.
+request={"messages":
+         [
+         {"role":"user","content":
+          [
+              {"text":"tell me a joke"}
+          ]
+         }
+],
+"inferenceConfig":{"maxTokens":512,"stopSequences":[],"temperature":0.7,"topP":0.9}}
+
+
+
 
 # Convert the native request to JSON.
-request = json.dumps(native_request)
+#request = json.dumps(titan_request)
+request = json.dumps(request)
 
 try:
     # Invoke the model with the request.
@@ -38,8 +49,11 @@ except (ClientError, Exception) as e:
 model_response = json.loads(response["body"].read())
 
 # Extract and print the response text.
-response_text = model_response["results"][0]["outputText"]
+#response_text = model_response["results"][0]["outputText"]
+print("")
 print("--------------------------------")
-print(response_text)
+
+
+print(model_response)
 
 
