317e627dfc7e796bf786806a384b45359c36abb4
wgreunke <wgreunke@gmail.com>
2025-01-03 21:51:34 -0800

    sd

--- gpt_image_reader.py
+++ gpt_image_reader.py
@@ -1,24 +1,39 @@
 #This sends an image to chat gpt and asks it to describe the image. 
-
+from key import API_KEY
 import openai
+import base64
 
-client = openai.OpenAI()
 
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "image",
-          "source": {
-            "type": "base64",
-            "media_type": "image/png",
-            "data": encoded_image
-          }
-        }
-      ]
-    }
-  ]
+image_path = "C:/Users/kahin/Downloads/16-af0075cl_1735282644.png"
+with open(image_path, 'rb') as image_file:
+    encoded_image = base64.b64encode(image_file.read()).decode()
+
+
+model = "gpt-4o-mini"
+prompt = "Tell me a joke"
+
+client = openai.OpenAI(api_key=API_KEY)
+
+#response = client.chat.completions.create(
+#    model=model,
+#    messages=[
+#        {"role": "system", "content": "You are a helpful assistant."},
+#        {"role": "user", "content": prompt}
+#    ]
+#)
+
+#Add the image content to payload
+response= client.chatt.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+        {"role": "user", "content": encoded_image}
+    ]
 )
+
+
+
+# Extract and return the response content
+
+print(response.choices[0].message.content.strip())
