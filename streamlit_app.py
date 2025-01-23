204f4db0cef26e6f648a54770929c75fc56b18d3
wgreunke <wgreunke@gmail.com>
2025-01-23 14:30:10 -0800

    Almost ready to show to customer.  Need to get some more days of data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,11 @@ import matplotlib.pyplot as plt
 
 
 
-"""
-Notes:
-Need to fix the datae time so that it is in pacific time.
 
+#Notes:
+#Need to fix the datae time so that it is in pacific time.
+#You are fetching all the images from the S3 bucket, even if they are not in the filtered data.  Need to improve this.
 
-
-"""
 
 
 
@@ -26,22 +24,10 @@ load_dotenv()
 aws_access_key_id = os.getenv('aws_access_key_id')
 aws_secret_access_key = os.getenv('aws_secret_access_key')
 
-#Connect to the S3 bucket
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)   
-#Get a list of all the files in the S3 bucket
-s3_objects = s3.list_objects_v2(Bucket='vendor-images-storage')  
-#st.write(s3_objects)
 
-
-#Show this image from the S3 bucket 1737154609_17-cn4065cl_Costco.png
-image_key = '1737154609_17-cn4065cl_Costco.png'
-image_url = s3.generate_presigned_url('get_object', Params={'Bucket': 'vendor-images-storage', 'Key': image_key}, ExpiresIn=3600)
-st.image(image_url)
 
 #Load the price tracker data from DynamoDB
 dynamodb_connection = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-
-
 
 
 #Get all the items from the table
@@ -55,30 +41,19 @@ dyanamo_response['Items'] = [
 #Convert the response to a pandas dataframe
 price_tracker_data = pd.DataFrame(dyanamo_response['Items'])
 
-#Pull the images from the S3 bucket
-for index, row in price_tracker_data.iterrows():
-    # Check if 'image_key' exists in the row
-    if 'image_name' in row:
-        image_name = row['image_name']
-        image_url = s3.generate_presigned_url('get_object', Params={'Bucket': 'vendor-images-storage', 'Key': image_name}, ExpiresIn=3600)
-        st.write(image_name)
-        st.image(image_url)
-    else:
-        st.warning(f"No image key found for index {index}.")  # Notify if the key is missing
-
 
 
 #Convert Unix timestamp to datetime only showing year month and day
 price_tracker_data['year_month_day'] = pd.to_datetime(price_tracker_data['pacific_datetime'], unit='s').dt.date
 
 #put the datetime column in the first position
-price_tracker_data = price_tracker_data[['year_month_day','pacific_datetime', 'product', 'channel', 'list_price', 'sale_amount', 'sale_price', 'url']]
+price_tracker_data = price_tracker_data[['year_month_day','pacific_datetime', 'product', 'channel', 'list_price', 'sale_amount', 'sale_price', 'url', 'image_name']]
 
 #Sort the data by the pacific_datetime2 column
 price_tracker_data = price_tracker_data.sort_values(by='pacific_datetime')
 
-st.write("modified table")
-st.write(price_tracker_data)
+#st.write("modified table")
+#st.write(price_tracker_data)
 
 
 # Add ability to filter by product name (update the column name to match your actual data)
@@ -101,14 +76,16 @@ filtered_data['sale_price'] = filtered_data['sale_price'].astype(float)
 #make list_price an integer
 filtered_data['list_price'] = filtered_data['list_price'].astype(float)
 
+
 plt.figure(figsize=(10, 6))
 
 # Plot lines
 plt.plot(filtered_data['pacific_datetime'], filtered_data['sale_price'], label='Sale Price')
 plt.plot(filtered_data['pacific_datetime'], filtered_data['list_price'], label='List Price')
 
+
 # Customize plot
-plt.title('Sale Price vs List Price Over Time')
+plt.title('Sale Price and List Price Over Time')
 plt.xlabel('Date')
 plt.ylabel('Price')
 plt.ylim(0, filtered_data['sale_price'].max() + 10)  # Add some padding
@@ -121,9 +98,36 @@ st.pyplot(plt)
 
 
 
+#************************ Handle Images *****************************
+#Connect to the S3 bucket
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)   
+#Get a list of all the files in the S3 bucket
+s3_objects = s3.list_objects_v2(Bucket='vendor-images-storage')  
+#st.write(s3_objects)
 
+
+
+
+#Pull the images from the S3 bucket
+for index, row in filtered_data.iterrows():
+    # Check if 'image_key' exists in the row
+    print(row['image_name'])
+    if 'image_name' in row:
+        image_name = row['image_name']
+        image_url = s3.generate_presigned_url('get_object', Params={'Bucket': 'vendor-images-storage', 'Key': image_name}, ExpiresIn=3600)
+        st.write(image_name)
+        st.write(row['year_month_day'])
+        st.image(image_url)
+    else:
+        st.warning(f"No image key found for index {index}.")  # Notify if the key is missing
+
+
+#Show this image from the S3 bucket 1737154609_17-cn4065cl_Costco.png
+#image_key = '1737154609_17-cn4065cl_Costco.png'
+#image_url = s3.generate_presigned_url('get_object', Params={'Bucket': 'vendor-images-storage', 'Key': image_key}, ExpiresIn=3600)
+#st.image(image_url)
 
 
 
 # Print column names to see what's available
-st.write("Available columns:", price_tracker_data.columns)
+#st.write("Available columns:", price_tracker_data.columns)
